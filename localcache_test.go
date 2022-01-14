a88c8350c0c411ca8957a74d5c93d77f5eb6778d
sado <liaoyonglin@shangbanzu.gift>
2022-01-14 16:26:18 +0800

    add cache lru

--- localcache_test.go
+++ localcache_test.go
@@ -141,3 +141,34 @@ func Test_TakeError(t *testing.T) {
 	assert.Equal(t, 0, cache.size())
 	assert.Equal(t, int32(1), atomic.LoadInt32(&counter))
 }
+
+func Test_WithLruEvicts(t *testing.T) {
+	cache, err := New(time.Second*3, WithLimit(3))
+	assert.Nil(t, err)
+
+	var (
+		ctx = context.Background()
+	)
+
+	cache.Set(ctx, "foo1", "bar1")
+	cache.Set(ctx, "foo2", "bar2")
+	cache.Set(ctx, "foo3", "bar3")
+	cache.Set(ctx, "foo4", "bar4")
+
+	get, ok := cache.Get(ctx, "foo1")
+	assert.False(t, ok)
+	assert.Nil(t, get)
+
+	get, ok = cache.Get(ctx, "foo2")
+	assert.True(t, ok)
+	assert.Equal(t, "bar2", get)
+
+	get, ok = cache.Get(ctx, "foo3")
+	assert.True(t, ok)
+	assert.Equal(t, "bar3", get)
+
+	get, ok = cache.Get(ctx, "foo4")
+	assert.True(t, ok)
+	assert.Equal(t, "bar4", get)
+
+}
